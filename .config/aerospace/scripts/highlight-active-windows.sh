04b21084eba6218d99db5cd1d63fb4d242809117
yougotwill <yougotwill94@gmail.com>
2025-01-23 19:13:19 +1100

    set alt tab to focus on windows

--- .config/aerospace/scripts/highlight-active-windows.sh
+++ .config/aerospace/scripts/highlight-active-windows.sh
@@ -2,4 +2,4 @@
 # Highlight focused windows with a colored border
 # Uses https://github.com/FelixKratz/JankyBorders
 
-/opt/homebrew/bin/borders active_color=0xff69D9F9 inactive_color=0x00494d64 width=7.0 blur_radius=8
+/opt/homebrew/bin/borders active_color=0xff69D9F9 inactive_color=0x00494d64 width=5.0 blur_radius=8
